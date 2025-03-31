fb35e844d4391bd0be949c9181dd436ffd6fd5e3
crozenboom <calebrozenboom01@gmail.com>
2025-03-31 14:43:13 -0500

    delete

--- Inventory/check_sllurp.py
+++
@@ -1,7 +0,0 @@
-import sllurp
-import pkgutil
-
-print("sllurp version:", sllurp.__version__)
-print("Available submodules:")
-for module in pkgutil.iter_modules(sllurp.__path__):
-    print(f" - {module.name}")
